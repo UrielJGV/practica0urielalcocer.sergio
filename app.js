805738fe9fd03efd9de8ba7626127688eea9bbce
PCSERGIO\uriel <urielalcocer.sergio@jgvaldemora.org>
2026-09-16 13:09:50 +0200

    potato

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("¡Hola DWEC! Mi taller ya funciona.");
+
+let contador = 0;
+
+const boton = document.getElementById("botonContador");
+const aviso = document.getElementById("aviso");
+const modo = document.getElementById("modonoche");
+
+if (boton) {
+    boton.addEventListener("click", function() {
+        contador++;
+
+        aviso.textContent =
+            "Has pulsado " + contador + " veces";
+    });
+}
+
+if (modo) {
+    modo.addEventListener("click", function() {
+        document.body.classList.toggle("tema-noche");
+    });
+}
